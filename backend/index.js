2a21d12a50605467b3096f205a59fc1960b1d12f
Michael Yeaple <michael@yeapleinnovations.com>
2019-03-17 20:08:33 -0400

    Update default backend port to 3001.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const server = require('./server')
 
-const port = process.env.BACKEND_PORT || 3000
+const port = process.env.BACKEND_PORT || 3001
 server.listen(port, () => console.log(`API server started on ${port}`))
